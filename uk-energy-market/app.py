db06e16c747c696ea109b0bcb4733924400180a6
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2026-01-20 21:47:07 +0000

    energy market app initial commit

---
+++ uk-energy-market/app.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import os
+
+
+class FileReader:
+    def __init__(self, fpath, fname):
+        self.fpath = fpath
+        self.fname = fname
+        self.dot = fname.find(".")
+        self.extension = fname[self.dot:].lower()
+        self.file = os.path.join(self.fpath, self.fname)
+
+    def read(self, sheet_name, header):
+        if self.extension == ".csv":
+            return pd.read_csv(self.file)
+        elif self.extension == ".xlsx":
+            return pd.read_excel(self.file, sheet_name=sheet_name, header=header)
+
+fdir = "C:\\Users\\samee\\Desktop\\py-projects\\other\\electricity\\datasets\\"
+files = [f for f in os.listdir(fdir)]
+# print(files)
+
+"""
+df_daily = pd.read_excel(fdir+files[0], sheet_name=3, header=3)
+df_monthly = pd.read_excel(fdir+files[0], sheet_name=4)
+
+df_daily.set_index("Date", inplace=True)
+# df_daily.plot()
+# plt.show()
+
+st.title("Energy Market Analysis")
+"""
+
+reader = FileReader(fdir, files[0])
+df = reader.read(3, 4)
+print(df.head())
+
