879ebc8d523457433ae30fafa185417cc0fb2321
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2026-01-25 21:13:43 +0000

    tweaks

--- uk-energy-market/app.py
+++ uk-energy-market/app.py
@@ -18,9 +18,16 @@ class FileReader:
         elif self.extension == ".xlsx":
             return pd.read_excel(self.file, sheet_name=sheet_name, header=header)
 
-fdir = "C:\\Users\\samee\\Desktop\\py-projects\\other\\electricity\\datasets\\"
-files = [f for f in os.listdir(fdir)]
-# print(files)
+
+if __name__ == "__main__":
+    fdir = "C:\\Users\\samee\\Desktop\\py-projects\\other\\electricity\\datasets\\"
+    files = [f for f in os.listdir(fdir)]
+
+    reader = FileReader(fdir, files[0])
+    df = reader.read(3, 3)
+
+    print(df.head(10))
+
 
 """
 df_daily = pd.read_excel(fdir+files[0], sheet_name=3, header=3)
@@ -33,7 +40,6 @@ df_daily.set_index("Date", inplace=True)
 st.title("Energy Market Analysis")
 """
 
-reader = FileReader(fdir, files[0])
-df = reader.read(3, 4)
-print(df.head())
+
+
 
